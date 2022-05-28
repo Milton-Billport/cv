924347d0ee6f1146af0b04d56a0482d10962fe51
nichol88 <nichol88@gmail.com>
2022-05-28 17:25:48 -0400

    add constants file to store global vars

---
+++ constants.js
@@ -0,0 +1,4 @@
+const FULL_NAME    = 'Brian Nicholls'
+const LINKEDIN_URL = 'https://www.linkedin.com/in/bnchlls/'
+const TWITTER_URL  = 'https://twitter.com/bfivegames'
+const WEBSITE_URL  = 'https://briannicholls.github.io/'
