0079181a1874690058b60e3a45f0782255b4026e
nichol88 <nichol88@gmail.com>
2022-05-28 17:32:41 -0400

    default global vars to empty strings

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const FULL_NAME    = 'Brian Nicholls'
-const LINKEDIN_URL = 'https://www.linkedin.com/in/bnchlls/'
-const TWITTER_URL  = 'https://twitter.com/bfivegames'
-const WEBSITE_URL  = 'https://briannicholls.github.io/'
+const FULL_NAME    = ''
+const LINKEDIN_URL = ''
+const TWITTER_URL  = ''
+const WEBSITE_URL  = ''
