a86fb28ae74e2a17b3a23e3ca7c63b2efc9a8356
nichol88 <nichol88@gmail.com>
2022-05-28 17:53:37 -0400

    change document title to full name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
   // Configure template with global vars
+  document.title = FULL_NAME
   document.getElementById('who-am-i').textContent += FULL_NAME.substring(0, FULL_NAME.indexOf(' '))
   document.getElementById('linkedin-url').href = LINKEDIN_URL
   document.getElementById('twitter-url').href  = TWITTER_URL
