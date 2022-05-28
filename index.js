ced19463a2546c09ced1f48e984d3a2f44c103e3
nichol88 <nichol88@gmail.com>
2022-05-28 17:50:11 -0400

    formatting

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', () => {
-  
   // Configure template with global vars
   document.getElementById('who-am-i').textContent += FULL_NAME.substring(0, FULL_NAME.indexOf(' '))
-  
   document.getElementById('linkedin-url').href = LINKEDIN_URL
   document.getElementById('twitter-url').href  = TWITTER_URL
   document.getElementById('website-url').href  = WEBSITE_URL
   document.getElementById('copyright').textContent += FULL_NAME
-
 })
